e51e6e2e471d600d1721129162f008397b3b5a25
rockbenben <qingwhat@gmail.com>
2023-06-15 22:17:37 +0800

    feat: Add optional variables to JsonWeightUpdater

--- src/pages/components/JsonWeightUpdater.tsx
+++ src/pages/components/JsonWeightUpdater.tsx
@@ -6,28 +6,30 @@ const JsonWeightUpdater = () => {
   const [countJson, setCountJson] = useState('');
   const [result, setResult] = useState('');
 
+  // 新增的 state
+  const [idField, setIdField] = useState('id');
+  const [weightField, setWeightField] = useState('weight');
+  const [cardIdField, setCardIdField] = useState('card_id');
+  const [countField, setCountField] = useState('count');
+
   const handleUpdate = () => {
     try {
       const originalData = JSON.parse(jsonInput);
       const countData = JSON.parse(countJson);
 
       const countDataMap = new Map(
-        countData.map(({ card_id, count }) => [card_id, count])
+        countData.map((item) => [item[cardIdField], item[countField]])
       );
 
       const updatedData = originalData.map((item) => {
         if (
-          item.title &&
-          (item.title.includes("AI DAN") ||
-            item.title.includes("DAN 10.0") ||
-            item.title.includes("DAN 11.0") ||
-            item.title.includes("无限制的 ChatGPT") ||
-            item.title.includes("失效"))
+          item.zh.title &&
+          (item.zh.title.includes("失效"))
         ) {
-          return { ...item, weight: 0 };
+          return { ...item, [weightField]: 0 };
         }
-        if (countDataMap.has(item.id.toString())) {
-          return { ...item, weight: countDataMap.get(item.id.toString()) };
+        if (countDataMap.has(item[idField].toString())) {
+          return { ...item, [weightField]: countDataMap.get(item[idField].toString()) };
         }
         return item;
       });
@@ -52,14 +54,18 @@ const JsonWeightUpdater = () => {
       type="secondary"
       style={{ fontSize: "14px", marginBottom: "20px" }}
     >
-      请在输入框中输入原始 JSON 数据和计数 JSON 数据，然后点击“匹配”按钮。工具会根据计数数据中的「card_id」与原始数据中的「id」匹配，并更新原始数据中的「weight」值。处理成功后，页面会显示处理后的内容，并在“匹配结果框”中显示结果。用户可以根据需要点击“复制结果”按钮，将结果复制到剪贴板。
+      请在输入框中输入原始 JSON 数据和计数 JSON 数据，然后点击“匹配”按钮。工具会根据计数数据中的「card_id」与原始数据中的「id」匹配，并将对应计数数据的「count」值更新到原始数据中的「weight」值。处理成功后，页面会显示处理后的内容，并在“匹配结果框”中显示结果。用户可以根据需要点击“复制结果”按钮，将结果复制到剪贴板。
       </Typography.Paragraph>
       <Row gutter={[16, 16]}>
         <Col xs={24} md={12}>
           <Input.TextArea placeholder="输入原始JSON数据" value={jsonInput} onChange={(e) => setJsonInput(e.target.value)} rows={5} />
+          <Input.TextArea placeholder="输入计数JSON数据" value={countJson} onChange={(e) => setCountJson(e.target.value)} rows={3}  style={{ marginTop: "5px" }}/>
         </Col>
         <Col xs={24} md={12}>
-          <Input.TextArea placeholder="输入计数JSON数据" value={countJson} onChange={(e) => setCountJson(e.target.value)} rows={3} />
+          <Input placeholder="原始数据 ID 字段名" value={idField} onChange={(e) => setIdField(e.target.value)} />
+          <Input placeholder="原始数据 weight 字段名" value={weightField} onChange={(e) => setWeightField(e.target.value)}  style={{ marginTop: "5px" }}/>
+          <Input placeholder="计数数据 card_id 字段名" value={cardIdField} onChange={(e) => setCardIdField(e.target.value)}  style={{ marginTop: "5px" }}/>
+          <Input placeholder="计数数据 count 字段名" value={countField} onChange={(e) => setCountField(e.target.value)}  style={{ marginTop: "5px" }}/>
           <Button onClick={handleUpdate} style={{ marginRight: "10px", marginTop: "10px" }}>更新</Button>
           <Button onClick={handleCopy} style={{ marginTop: "10px" }}>复制结果</Button>
         </Col>
