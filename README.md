e6546fcdce66826d58715f58f64784ab0623f456
rockbenben <qingwhat@gmail.com>
2023-12-19 15:11:40 +0800

    feat: Enhance JsonTranslate functionality

--- README.md
+++ README.md
@@ -30,13 +30,15 @@ Please provide all relevant code snippets for each task and explain how they wor
 
 ## 工具说明
 
+### JsonTranslate i18n 翻译器
+
+JsonTranslate 是一款高效的多语言 JSON 翻译工具，专为开发者和内容创作者设计。支持 Google Translate、DeepL 和 DeepLX 翻译 API，助力快速实现项目的国际化和本地化。无论您是在开发多语言网站、应用程序还是处理多语言数据集，JsonTranslate 都能提供简便的解决方案，轻松将 JSON 文件中的内容翻译为多种目标语言。了解更多可查看[使用教程](https://newzone.top/apps/devdocs/json-translate.html)。
+
+![](https://img.newzone.top/2023-12-18-16-09-04.gif?imageMogr2/format/webp "JsonTranslate 使用示例")
+
 ### 批量文本处理器
 
 批量文本处理器除了支持正则匹配和 JSON 节点提取，还可以在多条件匹配时使用条件语句执行不同的文本处理操作。例如，匹配 `<h4>` 和 `</h4>` 之间的文本执行文本处理操作 1，匹配 `<h5>` 和 `</h5>` 之间的文本执行文本处理操作 2，如果都没有匹配到则直接输出文本。批量文本处理器适合从大量数据中提取文本，并按照特定规则进行处理和输出。
-
-### i18n 翻译器
-
-本工具适用于 i18n 文件翻译或 JSON 等规则文本翻译，同时支持规则文本的批量处理。支持 Google Translate API、DeepL、DeepLX。
 
 ## Deploy
 
--- src/app/json-translate/page.tsx
+++ src/app/json-translate/page.tsx
@@ -2,9 +2,10 @@
 
 import React, { useState } from "react";
 import {
-  Row, Col, Button, Form, Typography, Input, Select, message, Card, Space, Spin
+  Row, Col, Button, Tooltip, Form, Typography, Input, Select, message, Card, Space, Spin
 } from "antd";
 import { JSONPath } from "jsonpath-plus";
+import _ from 'lodash';
 import { translateText } from '../components/translateText';
 import KeyMappingInput from "../components/KeyMappingInput";
 import Head from "next/head";
@@ -14,14 +15,20 @@ const { Title, Paragraph } = Typography;
 const JsonTranslate = () => {
   const [apiKey, setApiKey] = useState<string>("");
   const [jsonInput, setJsonInput] = useState<string>("");
-  const [jsonOutput, setJsonOutput] = useState<any>({});
-  const [translationMethod, setTranslationMethod] = useState<string>("google");
+  const [jsonOutput, setJsonOutput] = useState<string>("");
+  const [translationMethod, setTranslationMethod] = useState<string>("deeplx");
   const [sourceLanguage, setSourceLanguage] = useState<string>("en");
   const [targetLanguage, setTargetLanguage] = useState<string>("zh");
+  const [showSimpleInput, setShowSimpleInput] = useState(true);
+  const [simpleInputKey, setSimpleInputKey] = useState("");
+  const toggleInputType = () => {
+    setShowSimpleInput(!showSimpleInput);
+  };
   const [keyMappings, setKeyMappings] = useState<
     Array<{ inputKey: string; outputKey: string }>
   >([{ inputKey: "", outputKey: "" }]);
   const [isLoading, setIsLoading] = useState(false);
+  const [jsonInputError, setJsonInputError] = useState(null);
 
   const translationMethods = [
     { value: "google", label: "Google Translate" },
@@ -35,24 +42,47 @@ const JsonTranslate = () => {
   };
 
   const languages: LanguageOption[] = [
-    { value: "en", label: "English" },
-    { value: "zh", label: "中文" },
-    { value: "ja", label: "日语" },
-    { value: "ko", label: "韩语" },
+    { value: "en", label: "英语（English）" },
+    { value: "zh", label: "中文（Chinese）" },
+    { value: "ja", label: "日本語" },
+    { value: "ko", label: "韩语（한국어）" },
     { value: "es", label: "西班牙语" },
-    { value: "fr", label: "法语" },
-    { value: "de", label: "德语" },
+    { value: "fr", label: "法语（Français）" },
+    { value: "de", label: "德语（Deutsch）" },
     { value: "it", label: "意大利语" },
-    { value: "ru", label: "俄语" },
+    { value: "ru", label: "俄语（Русский）" },
     { value: "pt", label: "葡萄牙语" },
     { value: "hi", label: "印地语 (仅 Google)" },
     { value: "ar", label: "阿拉伯语 (仅 Google)" },
     { value: "bn", label: "孟加拉语 (仅 Google)" },
   ];
 
+  // Parse JSON Input, set error if invalid
+  const parseAndSetJsonInput = (input) => {
+    try {
+      const parsedJson = JSON.parse(input);
+      setJsonInput(JSON.stringify(parsedJson, null, 2));
+      setJsonInputError(null);
+    } catch (error) {
+      setJsonInputError("JSON Input 格式错误，请检查");
+    }
+  };
+
+  const debouncedParseAndSetJsonInput = _.debounce(parseAndSetJsonInput, 1500);
+
+  const handleJsonInputChange = (e) => {
+    const input = e.target.value;
+    setJsonInput(input);
+    debouncedParseAndSetJsonInput(input);
+  };
+
   const handleTranslate = async () => {
+    // Reset Output
+    setJsonOutput("");
+
+    // Check Input
     if (!apiKey && translationMethod !== "deeplx") {
-      message.error("Google/DeepL 翻译方法中，API Key 不能为空");
+      message.error("Google/DeepL 翻译方法中，API Key 不能为空。没有 API 的话，可以使用 DeepLX 免费翻译。");
       return;
     }
     if (!jsonInput) {
@@ -68,59 +98,83 @@ const JsonTranslate = () => {
       return;
     }
     setIsLoading(true);
-    const translations = keyMappings.map(async (mapping) => {
-      if (!mapping.inputKey || !mapping.outputKey) {
-        return;
-      }
-      const inputNodes = JSONPath({ path: `$..${mapping.inputKey}`, json: jsonObject, resultType: 'all' });
-      const outputNodes = JSONPath({ path: `$..${mapping.outputKey}`, json: jsonObject, resultType: 'all' });
 
-      if (inputNodes.length === 0) {
-        message.error(`输入键 ${mapping.inputKey} 在 JSON 中找不到`);
-        return;
+    try {
+      let mappings;
+      if (showSimpleInput) {
+        // 替换中文逗号为英文逗号，并分割
+        const keys = simpleInputKey.replace(/，/g, ',').split(',').filter(k => k.trim() !== '');
+        mappings = keys.map(key => ({ inputKey: key.trim(), outputKey: key.trim() }));
+      } else {
+        mappings = keyMappings;
       }
-      if (outputNodes.length === 0) {
-        message.error(`输出键 ${mapping.outputKey} 在 JSON 中找不到`);
-        return;
-      }
+      // 一次性处理所有 JSONPath 查询
+      const inputNodesMap = new Map();
+      const outputNodesMap = new Map();
 
-      const tasks = inputNodes.map(async (node, index) => {
-        const translatedText = await translateText({
-          text: node.value,
-          translationMethod,
-          targetLanguage,
-          sourceLanguage,
-          apiKey
-        });
-        // 应用翻译文本
-        let outputNodePathArray = JSONPath.toPathArray(outputNodes[index].path);
-        if (outputNodePathArray && outputNodePathArray.length > 0) {
-          let currentNode = jsonObject;
-          for (let i = 1; i < outputNodePathArray.length - 1; i++) {
-            currentNode = currentNode[outputNodePathArray[i]];
-          }
-          currentNode[outputNodePathArray[outputNodePathArray.length - 1]] = translatedText;
+      for (const { inputKey, outputKey } of mappings) {
+        if (!inputKey || !outputKey) {
+          throw new Error(`输入或输出键缺失`);
         }
-      });
 
-      // 等待所有的翻译任务完成
-      await Promise.all(tasks);
-    });
+        if (!inputNodesMap.has(inputKey)) {
+          inputNodesMap.set(inputKey, JSONPath({ path: `$..${inputKey}`, json: jsonObject, resultType: 'all' }));
+        }
+        if (!outputNodesMap.has(outputKey)) {
+          outputNodesMap.set(outputKey, JSONPath({ path: `$..${outputKey}`, json: jsonObject, resultType: 'all' }));
+        }
 
-    // 等待所有的键映射完成
-    await Promise.all(translations);
+        const inputNodes = inputNodesMap.get(inputKey);
+        const outputNodes = outputNodesMap.get(outputKey);
 
-    setJsonOutput(JSON.stringify(jsonObject, null, 2));
-    setIsLoading(false);
+        if (inputNodes.length === 0 || outputNodes.length === 0) {
+          throw new Error(`输入键 ${inputKey} 或输出键 ${outputKey} 在 JSON 中找不到`);
+        }
+        if (inputNodes.length !== outputNodes.length) {
+          throw new Error(`输入键 ${inputKey} 和输出键 ${outputKey} 的节点数量不匹配`);
+        }
+
+        const tasks = inputNodes.map(async (node, index) => {
+          const translatedText = await translateText({
+            text: node.value,
+            translationMethod,
+            targetLanguage,
+            sourceLanguage,
+            apiKey
+          });
+          applyTranslation(jsonObject, outputNodes[index].path, translatedText);
+        });
+
+        await Promise.all(tasks);
+      }
+
+      setJsonOutput(JSON.stringify(jsonObject, null, 2));
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const applyTranslation = (jsonObject, path, translatedText) => {
+    const outputNodePathArray = JSONPath.toPathArray(path);
+    if (!outputNodePathArray || outputNodePathArray.length === 0) {
+      throw new Error(`输出路径解析错误`);
+    }
+    let currentNode = jsonObject;
+    for (let i = 1; i < outputNodePathArray.length - 1; i++) {
+      currentNode = currentNode[outputNodePathArray[i]];
+    }
+    currentNode[outputNodePathArray[outputNodePathArray.length - 1]] = translatedText;
   };
 
   const handleCopyResult = () => {
     navigator.clipboard.writeText(jsonOutput).then(
       () => {
-        message.success("结果已复制到剪贴板");
+        message.success("翻译结果已复制到剪贴板");
       },
       (err) => {
-        message.error("无法复制结果，请手动复制");
+        message.error("无法复制翻译内容，请手动复制");
       }
     );
   };
@@ -129,40 +183,27 @@ const JsonTranslate = () => {
   return (
     <>
       <Head>
-        <title> JSON 节点机器翻译器 - 可用于 i18n 国际化 | Tools by AI</title>
+        <title>多语言 JSON 翻译工具 - i18n 国际化开发助手 | JsonTranslate | Tools by AI</title>
         <meta
           name='description'
-          content='强大的JSON节点翻译工具，允许用户指定JSON输入的节点进行翻译，它可以通过Google或DeepL的翻译API将选定的节点从源语言翻译为目标语言，并将翻译的结果填入相应的输出节点。'
+          content='JsonTranslate 是一个轻便的多语言 JSON 翻译工具，专为软件开发者和内容管理者设计，以支持国际化和本地化项目。轻松转换 JSON 文档中的文本到多种目标语言，使用 Google Translate、DeepL 和 DeepLX 翻译 API。'
         />
         <meta
           name='keywords'
-          content='JSON, 翻译器, JSON翻译, 语言转换, 机器翻译, 自动翻译, JSON节点,Google翻译API, DeepL翻译API, DeepLX 免费翻译'
+          content='JsonTranslate, JSON 翻译工具, 多语言翻译, 国际化工具, i18n, 本地化, Google 翻译 API, DeepL 翻译 API, 自动翻译 JSON, 编程语言翻译'
         />
       </Head>
       <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "24px" }}>
         <Title level={3} style={{ marginBottom: "24px" }}>
-          JSON 节点机器翻译器
+          多语言 JSON 翻译工具
         </Title>
         <Paragraph
           type='secondary'
           style={{ fontSize: "14px", marginBottom: "20px" }}
-          ellipsis={ellipsis ? { rows: 2, expandable: true, symbol: 'more' } : false}
+          ellipsis={ellipsis ? { rows: 3, expandable: true, symbol: 'more' } : false}
         >
-          本工具旨在帮助用户处理和翻译 JSON 数据。它使用 Google/DeepL Translate API，能够将用户指定的 JSON 输入中的某些节点的值翻译成目标语言，并将翻译结果填入相应的输出节点中。用户可以自定义输入/输出节点，选择源语言和目标语言，然后通过简单的点击操作完成翻译任务。
-          <br />如果你尚未注册 API，可参考
-          <a href='https://docs.easyuseai.com/platform/translate/google_fanyi.html'>
-            接口申请教程
-          </a>
-          。如果已成功申请，可以直接查看{" "}
-          <a href='https://console.cloud.google.com/apis/credentials/key/2c5756a5-5a4c-4d48-993f-e478352dcc64?project=ordinal-nucleus-383814'>
-            Google Translate API
-          </a>{" "}
-          或 <a href='https://www.deepl.com/zh/account/summary'>DeepL API</a>。使用建议：对于界面 UI 翻译，建议使用 Google Translate；而对于长句翻译，DeepL 的质量更好。
-          请注意，Google Translate 会直接将数据发送到 Google，然而 DeepL API
-          并不支持网页使用。因此，我在服务器上设立了一个转发接口。服务器只负责转发你的数据，不会进行数据收集。你也可以选择在本地端进行部署使用。
-          <br />
-          如果你只需进行轻度翻译，可以使用我搭建的 DeepLX 接口，它免费且无需添加 API。
-          <a href='https://github.com/OwO-Network/DeepLX/'>DeepLX</a> 是基于 DeepL 的免费开源项目，可以将 DeepL Free API 转换为本地 API，以供第三方程序使用。
+          JsonTranslate 是一款高效的多语言 JSON 翻译工具，专为开发者和内容创作者设计。支持 Google Translate、DeepL 和 DeepLX 翻译 API，助力快速实现项目的国际化和本地化。无论你是在开发多语言网站、应用程序还是处理多语言数据集，JsonTranslate 都能提供简便的解决方案，轻松将 JSON 文件中的内容翻译为多种目标语言。<br />了解更多：<a href='https://newzone.top/apps/devdocs/json-translate.html'>使用教程</a>；<a href='https://console.cloud.google.com/apis/credentials/key/2c5756a5-5a4c-4d48-993f-e478352dcc64?project=ordinal-nucleus-383814'>
+            Google Translate API</a>；<a href='https://www.deepl.com/zh/account/summary'>DeepL API</a>。
         </Paragraph>
         <Row gutter={16}>
           <Col xs={24} lg={12}>
@@ -199,17 +240,32 @@ const JsonTranslate = () => {
                 </Form.Item>
               </Space>
 
-              <KeyMappingInput
-                keyMappings={keyMappings}
-                setKeyMappings={setKeyMappings}
-              />
+              {
+                showSimpleInput ? (
+                  <Form.Item label='翻译键名'>
+                    <Input
+                      value={simpleInputKey}
+                      onChange={(e) => setSimpleInputKey(e.target.value)}
+                      placeholder='输入欲翻译的节点键名'
+                    />
+                  </Form.Item>
+                ) : (
+                  <KeyMappingInput
+                    keyMappings={keyMappings}
+                    setKeyMappings={setKeyMappings}
+                  />
+                )
+              }
               <Form.Item>
                 <Input.TextArea
-                  placeholder='JSON Input'
+                  placeholder='JSON Input，输入要翻译的 JSON'
                   value={jsonInput}
-                  onChange={(e) => setJsonInput(e.target.value)}
+                  onChange={handleJsonInputChange}
                   rows={10}
                 />
+                {jsonInputError && (
+                  <div style={{ color: "red" }}>{jsonInputError}</div>
+                )}
               </Form.Item>
             </Card>
           </Col>
@@ -218,13 +274,23 @@ const JsonTranslate = () => {
               <Spin spinning={isLoading}>
                 <Button
                   onClick={handleTranslate}
-                  style={{ marginBottom: "16px" }}>
+                  style={{ marginBottom: "16px", backgroundColor: '#1890ff', color: '#fff' }}
+                  onMouseOver={(e) => e.currentTarget.style.backgroundColor = '#40a9ff'}
+                  onMouseOut={(e) => e.currentTarget.style.backgroundColor = '#1890ff'}>
                   开始翻译
                 </Button>
                 <Button
                   onClick={handleCopyResult}
-                  style={{ marginLeft: "16px", marginBottom: "16px" }}>
-                  复制结果
+                  style={{ marginLeft: "16px", marginRight: "16px", marginBottom: "16px" }}>
+                  复制翻译结果
+                </Button>
+                <Button onClick={toggleInputType} style={{ backgroundColor: '#f5f5f5', color: 'rgba(0, 0, 0, 0.65)' }}>
+                  <Tooltip
+                    title="点击以切换翻译节点模式。单一键名模式表示翻译的输入输出使用相同节点，而映射翻译则涉及不同节点。例如，节点 A 的值将翻译至节点 B，节点 C 的值则翻译至节点 D。"
+                    placement="top"
+                  >
+                    {showSimpleInput ? '切换到映射翻译' : '切换到单一键名翻译'}
+                  </Tooltip>
                 </Button>
                 <Form.Item>
                   <Input.TextArea
