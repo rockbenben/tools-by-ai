82815b3c474fcc6419c941649bdb4db59c4c9054
rockbenben <qingwhat@gmail.com>
2024-06-07 15:37:49 +0800

    style(layout): move styles to globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,14 +5,6 @@ import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { GoogleTagManager } from "@next/third-parties/google";
 import AdsenseAd from "./AdSense";
 
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Tools by AI",
-  description: "由 AI 辅助生成的多种文本工具",
-  keywords: "ai tools",
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh-Hans">
@@ -20,10 +12,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <Navigation />
         <AntdRegistry>
-          <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "24px" }}>
+          <main>
             {children}
             <AdsenseAd />
-          </div>
+          </main>
         </AntdRegistry>
       </body>
     </html>
