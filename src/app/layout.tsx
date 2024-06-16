42c8c18f87290dd7d9c3f061aa9415ebbc38eae1
rockbenben <qingwhat@gmail.com>
2024-06-16 22:27:39 +0800

    perf: remove redundant code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,16 @@ import "./globals.css";
 import { Navigation } from "./ui/Navigation";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { GoogleTagManager } from "@next/third-parties/google";
-import AdsenseAd from "./AdSense";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh-Hans">
-      <GoogleTagManager gtmId="GTM-WBM6XHGB" />
+      <GoogleTagManager gtmId="GTM-YourTag" />
       <body>
         <Navigation />
         <AntdRegistry>
           <main>
             {children}
-            <AdsenseAd />
           </main>
         </AntdRegistry>
       </body>
